d67fe458f59d82f648eab9257e1231c7ac6968a5
kenta-s <knt01222@gmail.com>
2018-09-08 14:23:33 +0900

    React

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
+require('babel-core/register');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   mode: 'production'
 };
