c32f0d3f17a8d3952d25b65422ef9643b4c460e3
kenta-s <knt01222@gmail.com>
2018-09-09 11:04:12 +0900

    Make it work

--- src/index.jsx
+++ src/index.jsx
@@ -11,32 +11,70 @@ const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 const Handle = Slider.Handle;
 
-const handle = (props) => {
-  const { value, dragging, index, ...restProps } = props;
-  return (
-    <Tooltip
-      prefixCls="rc-slider-tooltip"
-      overlay={value}
-      visible={dragging}
-      placement="top"
-      key={index}
-    >
-      <Handle value={value} {...restProps} />
-    </Tooltip>
-  );
-};
+// const handle = (props) => {
+//   const { value, dragging, index, ...restProps } = props;
+//   return (
+//     <Tooltip
+//       prefixCls="rc-slider-tooltip"
+//       overlay={value}
+//       visible={dragging}
+//       placement="top"
+//       key={index}
+//     >
+//       <Handle value={value} {...restProps} />
+//     </Tooltip>
+//   );
+// };
 
 const wrapperStyle = { width: 400, margin: 50 };
+class Shitennoh extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      buget: 0,
+      quality: 0,
+      scope: 0
+    };
+  }
+
+  handle(props) {
+    const { value, dragging, index, ...restProps } = props;
+    return (
+      <Tooltip
+        prefixCls="rc-slider-tooltip"
+        overlay={value}
+        visible={dragging}
+        placement="top"
+        key={index}
+      >
+        <Handle value={value} {...restProps} />
+      </Tooltip>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <div style={wrapperStyle}>
+          <p>Slider with custom handle</p>
+          <Slider min={0} max={10} defaultValue={0} handle={this.handle} />
+
+          <p>Slider with custom handle</p>
+          <Slider min={0} max={10} defaultValue={0} handle={this.handle} />
+
+          <p>Slider with custom handle</p>
+          <Slider min={0} max={10} defaultValue={0} handle={this.handle} />
+
+          <p>Slider with custom handle</p>
+          <Slider min={0} max={10} defaultValue={0} handle={this.handle} />
+        </div>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <div>
-    <div style={wrapperStyle}>
-      <p>Slider with custom handle</p>
-      <Slider min={0} max={20} defaultValue={3} handle={handle} />
-    </div>
-    <div style={wrapperStyle}>
-      <p>Range with custom handle</p>
-      <Range min={0} max={20} defaultValue={[3, 10]} tipFormatter={value => `${value}%`} />
-    </div>
-  </div>,
+  <Shitennoh />,
   document.getElementById('root')
 );
